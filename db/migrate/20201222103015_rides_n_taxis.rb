a3fe46fa40674f6e9582176e6047f1ca08f08169
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-22 10:31:18 +0000

    Automatically backed up by Learn

--- db/migrate/20201222103015_rides_n_taxis.rb
+++ db/migrate/20201222103015_rides_n_taxis.rb
@@ -2,7 +2,7 @@ class RidesNTaxis < ActiveRecord::Migration[5.0]
   def change
     create_table :rides do |t|
     taxi_id:integer
-    passenger_id:integer 
+    passenger_id:integer
     end
   end
 end
