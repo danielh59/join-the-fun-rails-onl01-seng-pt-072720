1a8a584045678c90e35a822f9f31c192020d38a1
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-22 08:41:07 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,7 +2,6 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.integer :passenger_id
-      t.integer :taxi_id
       t.timestamps null: false
     end
   end
